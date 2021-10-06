e209d145e4876dafa635a4736de34d6a402a9418
Antonio <antonio.rodriguez024@gmail.com>
2021-10-06 12:18:20 -0500

    Model classes created and implemented in DB

--- src/main/java/net/javaguides/springboot/RegistrationLoginSpringBootSecurityThymeleafApplication.java
+++ src/main/java/net/javaguides/springboot/RegistrationLoginSpringBootSecurityThymeleafApplication.java
@@ -1,4 +1,4 @@
-package net.javaguides.sms;
+package net.javaguides.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
