c4d3524aeb8f9bcd8d7c40541f20ceca5fc5443c
Antonio <antonio.rodriguez024@gmail.com>
2021-10-07 01:28:31 -0500

    registration feature

--- src/main/java/net/javaguides/springboot/web/UserRegistrationController.java
+++ src/main/java/net/javaguides/springboot/web/UserRegistrationController.java
@@ -1,7 +1,9 @@
 package net.javaguides.springboot.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.javaguides.springboot.service.UserService;
@@ -18,9 +20,20 @@ public class UserRegistrationController {
 		this.userService = userService;
 	}
 	
+	@ModelAttribute("user")
+	public UserRegistrationDto userRegistrationDto() {
+		return new UserRegistrationDto();
+	}
+	
+	@GetMapping
+	public String showRegistrationForm() {
+		return "registration";
+	}
+	
+	@PostMapping
 	public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
 		userService.save(registrationDto);
-		return "redirect:/registration?success";
-		
+		return "redirect:/registration?success";	
 	}
+	
 }
